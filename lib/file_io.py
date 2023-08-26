e6cc298ee89c3551449af3cd27d1337752c60767
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-08-26 13:57:02 +0300

    completed assingment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, "w") as f:
+        f.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, "a") as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, "r") as f:
+        return f.read()
     pass
